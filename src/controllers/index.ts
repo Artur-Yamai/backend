c897953f1d1acfbf0910573555bdda0c90d1ba60
Iamaletdinov Artur <wbf.robit@gmail.com>
2023-07-17 15:10:44 +0500

    изменена таблица ratings на tobacco_ratings

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,5 +2,5 @@ export * as UserController from "./UserController";
 export * as TobaccoController from "./TobaccoController";
 export * as CommentController from "./CommentController";
 export * as FavoritesTobaccoController from "./FavoritesTobaccoController";
-export * as RatingController from "./RatingController";
+export * as TobaccoRatingController from "./ratings";
 export * as ReferenceController from "./ReferenceController";
