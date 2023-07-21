bfd11657d598133584aedce09454b611b7d84153
Iamaletdinov Artur <wbf.robit@gmail.com>
2023-07-21 15:47:42 +0500

    Добавлен RightsCheck, для проверки доступа прав

---
+++ src/validations/RightsCheck.ts
@@ -0,0 +1,47 @@
+import { NextFunction, Request, Response } from "express";
+import db from "../models";
+import responseHandler from "../utils/responseHandler";
+
+export enum RoleCodes {
+  user = 0,
+  moderator = 5,
+  admin = 10,
+}
+
+export default async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+  roleCode: RoleCodes
+): Promise<void> => {
+  try {
+    const userId = req.headers.userId;
+
+    const queryResult = await db.query(
+      `
+        SELECT role_code AS "roleCode" 
+        FROM hookah.user 
+        WHERE user_id = $1
+      `,
+      [userId]
+    );
+
+    if (!queryResult.rows[0]) {
+      const logText: string = `Пользователь ${userId} обнаружен`;
+      const respMessage: string = "Уровень прав доступа не обнаружен";
+      return responseHandler.notFound(req, res, logText, respMessage);
+    }
+
+    const userRoleCode = queryResult.rows[0].roleCode;
+
+    if (userRoleCode >= roleCode) {
+      return next();
+    }
+
+    const exeptionText: string = `Нет доступа для userId - ${userId}`;
+    const message: string = "Недостаточный уровень доступа";
+    responseHandler.exception(req, res, 403, exeptionText, message);
+  } catch (error) {
+    responseHandler.error(req, res, error, "Ошибка поиска прав пользователя");
+  }
+};
