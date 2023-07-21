40839bbcb8f5f63f3f0be7d9c8e50e2b35a1d755
Iamaletdinov Artur <wbf.robit@gmail.com>
2023-07-21 15:49:01 +0500

    В некоторые WebAPI добавлен middlewhere проверки уровня доступа прав

--- src/routes/referenceRoutes.ts
+++ src/routes/referenceRoutes.ts
@@ -1,7 +1,8 @@
-import { Router } from "express";
+import { Router, NextFunction, Request, Response } from "express";
 import multer from "multer";
 import { checkAuth } from "../utils";
 import { ReferenceController } from "../controllers";
+import RightsCheck, { RoleCodes } from "../validations/RightsCheck";
 
 const router = Router();
 const upload: multer.Multer = multer();
@@ -9,8 +10,25 @@ const upload: multer.Multer = multer();
 router
   .route("/api/reference/:name")
   .get(checkAuth, ReferenceController.getAll)
-  .post(checkAuth, upload.none(), ReferenceController.create)
-  .put(checkAuth, upload.none(), ReferenceController.update)
-  .delete(checkAuth, ReferenceController.remove);
+  .post(
+    checkAuth,
+    (req: Request, res: Response, next: NextFunction) =>
+      RightsCheck(req, res, next, RoleCodes.admin),
+    upload.none(),
+    ReferenceController.create
+  )
+  .put(
+    checkAuth,
+    (req: Request, res: Response, next: NextFunction) =>
+      RightsCheck(req, res, next, RoleCodes.admin),
+    upload.none(),
+    ReferenceController.update
+  )
+  .delete(
+    checkAuth,
+    (req: Request, res: Response, next: NextFunction) =>
+      RightsCheck(req, res, next, RoleCodes.admin),
+    ReferenceController.remove
+  );
 
 export { router };
