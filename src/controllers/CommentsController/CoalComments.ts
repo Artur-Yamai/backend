8e000585fe6905f4eb6aeb9b0da6a56e48f918f6
Iamaletdinov Artur <wbf.robit@gmail.com>
2023-08-03 10:22:08 +0500

    Добавлены WebAPI для комментариев угля

---
+++ src/controllers/CommentsController/CoalComments.ts
@@ -0,0 +1,110 @@
+import { Request, Response } from "express";
+import { v4 as uuidv4 } from "uuid";
+import db, { CommentModels } from "../../models";
+import responseHandler from "../../utils/responseHandler";
+
+export const create = async (req: Request, res: Response): Promise<void> => {
+  const userId = req.headers.userId;
+  const { coalId, text } = req.body;
+
+  try {
+    const queryResult = await db.query(CommentModels.create("coal"), [
+      uuidv4(),
+      userId,
+      coalId,
+      text,
+    ]);
+
+    const comment = queryResult.rows[0];
+
+    const message = "Комментарий успешно сохранен";
+    responseHandler.success(
+      req,
+      res,
+      201,
+      `commentId - ${comment.id} : ${message}`,
+      {
+        success: true,
+        message,
+        body: {
+          id: comment.id,
+        },
+      }
+    );
+  } catch (error: any) {
+    if (error?.detail?.indexOf("already exists") > -1) {
+      const message = "Нельзя оставлять более одного комментария";
+      return responseHandler.exception(
+        req,
+        res,
+        406,
+        `userId - ${userId} : попытка добавления более одного комментария для табака - ${coalId}`,
+        message
+      );
+    }
+
+    responseHandler.error(req, res, error, "Комментарий не был создан");
+  }
+};
+
+export const update = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { text, id } = req.body;
+    const userId = req.headers.userId;
+
+    const queryResult = await db.query(CommentModels.update("coal"), [
+      text,
+      id,
+      userId,
+    ]);
+
+    const comment = queryResult.rows[0];
+
+    if (!comment?.id) {
+      const respMessage = "Комментарий не найден";
+      return responseHandler.notFound(req, res, respMessage, respMessage);
+    }
+
+    const logText = `userId - ${userId} updated comment by ${comment.entityType} with id - ${comment.entityId}`;
+    responseHandler.success(req, res, 201, logText, {
+      success: true,
+      message: "Комментарий успешно обновлен",
+      body: comment,
+    });
+  } catch (error) {
+    responseHandler.error(req, res, error, "Комментарий не был создан");
+  }
+};
+
+export const remove = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const id = req.body.id;
+    const userId = req.headers.userId;
+
+    const queryResult = await db.query(CommentModels.remove("coal"), [id]);
+
+    const comment = queryResult.rows[0];
+
+    if (!comment?.comment_id) {
+      const respMessage = "Такой комментарий не найден";
+      const logText = `комментарий - ${id} юзера - ${userId} - ${respMessage}`;
+      return responseHandler.notFound(req, res, logText, respMessage);
+    }
+
+    await db.query(CommentModels.saveDeletedComment(), [
+      uuidv4(), // deleted_id
+      comment.comment_id,
+      comment.user_id,
+      comment.coal_id,
+      "coal",
+      comment.comment_text,
+      comment.created_at,
+      comment.updated_at,
+    ]);
+
+    const logText = `userId - ${userId} удалил комментарий табака - ${id}`;
+    responseHandler.forRemoved(req, res, logText);
+  } catch (error) {
+    responseHandler.error(req, res, error, "Комментарий не был создан");
+  }
+};
