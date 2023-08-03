6bb8392dcf6383aaec08189b045f1d87a88e7222
Iamaletdinov Artur <wbf.robit@gmail.com>
2023-08-03 05:13:34 +0500

    Добавлены CRUD-методы рейтинга табака

--- src/controllers/RatingsController/CoalRating.ts
+++ src/controllers/RatingsController/CoalRating.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import multer from "multer";
+import db, { RatingModels } from "../../models";
 import responseHandler from "../../utils/responseHandler";
 
 const upload: multer.Multer = multer();
@@ -11,7 +12,29 @@ export const add = [
       const userId = req.headers.userId;
       const { coalId, rating } = req.body;
 
-      res.json({ userId, coalId, rating });
+      const queryResult = await db.query(RatingModels.add("coal"), [
+        userId,
+        coalId,
+        rating,
+      ]);
+
+      if (queryResult.rowCount) {
+        const message = "Оценка поставлена";
+        responseHandler.success(
+          req,
+          res,
+          201,
+          `userId - ${userId} поставил coalId - ${coalId} оценку ${rating}`,
+          {
+            success: true,
+            message,
+          }
+        );
+      } else {
+        const respMessage = "Оценка не поставлена";
+        const logText = `userId - ${userId} не смог поставить coalId - ${coalId} оценку ${rating}`;
+        responseHandler.notFound(req, res, logText, respMessage);
+      }
     } catch (error) {
       responseHandler.error(req, res, error, "Оценка не была сохранена");
     }
@@ -25,7 +48,30 @@ export const update = [
       const userId = req.headers.userId;
       const { coalId, rating } = req.body;
 
-      res.json({ userId, coalId, rating });
+      const queryResult = await db.query(RatingModels.update("coal"), [
+        userId,
+        coalId,
+        rating,
+      ]);
+
+      if (queryResult.rowCount) {
+        const message = "Оценка изменена";
+
+        responseHandler.success(
+          req,
+          res,
+          201,
+          `userId - ${userId} изменил оценку у coalId - ${coalId} на ${rating}`,
+          {
+            success: true,
+            message,
+          }
+        );
+      } else {
+        const respMessage = "Оценка не поставлен";
+        const logText = `userId - ${userId} не смог изменить оценку у coalId - ${coalId} на ${rating}`;
+        responseHandler.notFound(req, res, logText, respMessage);
+      }
     } catch (error) {
       responseHandler.error(req, res, error, "Оценка не была изменена");
     }
@@ -37,7 +83,19 @@ export const remove = async (req: Request, res: Response): Promise<void> => {
     const userId = req.headers.userId;
     const coalId = req.body.id;
 
-    res.json({ userId, coalId });
+    const queryResult = await db.query(RatingModels.remove("coal"), [
+      userId,
+      coalId,
+    ]);
+
+    if (queryResult.rowCount) {
+      const logText = `userId - ${userId} удалил оценку к табака - ${coalId}`;
+      responseHandler.forRemoved(req, res, logText);
+    } else {
+      const logText = `userId - ${userId} не смог удалить оценку у coalId - ${coalId}`;
+      const respMessage = "Оценка не удалена";
+      responseHandler.notFound(req, res, logText, respMessage);
+    }
   } catch (error) {
     responseHandler.error(req, res, error, "Оценка не была удалена");
   }
