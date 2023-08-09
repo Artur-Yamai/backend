f000150795e28b58f21de4ad6f2b1731be52ce23
Iamaletdinov Artur <wbf.robit@gmail.com>
2023-08-09 17:06:08 +0500

    из корневого индексного файла можно получить html страницу

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import { avatarsDirName, tobaccoDirName, coalDirName } from "./constants";
 import {
@@ -14,15 +14,14 @@ import "./utils/PGChangeTypes";
 
 const port: number = 6060;
 const app: express.Express = express();
+const root = { root: __dirname + "/../dist" };
+
 app.use(express.json());
 app.use("/uploads/avatars", express.static(avatarsDirName));
 app.use("/uploads/tobaccos", express.static(tobaccoDirName));
 app.use("/uploads/coals", express.static(coalDirName));
+// app.get("/static/*", express.static("/dist/client"));
 app.use(cors());
-
-app.get("/", (req, res) => {
-  res.send("<h1>In process</h1>");
-});
 
 app.use(UserRouter);
 app.use(TobaccoRoutes);
@@ -32,6 +31,15 @@ app.use(ratingRoutes);
 app.use(referenceRoutes);
 app.use(coalRouter);
 
-app.listen(port, () => {
-  console.log(`http://localhost:${port}`);
+app.get("/static/:dirName/:fileName", (req: Request, res: Response) => {
+  const { dirName, fileName } = req.params;
+  res.sendFile(`client/static/${dirName}/${fileName}`, root);
 });
+
+app.get("/manifest.json", (_, res: Response) =>
+  res.sendFile(`client/manifest.json`, root)
+);
+
+app.get("*", (_, res: Response) => res.sendFile("/client/index.html", root));
+
+app.listen(port, () => console.log(`http://localhost:${port}`));
