f81c9dab461639c6eb38b773b2a2118fc1776257
Iamaletdinov Artur <wbf.robit@gmail.com>
2023-08-21 17:40:40 +0500

    multer вынесен в файл роутов рейтинга

--- src/routes/ratingRoutes.ts
+++ src/routes/ratingRoutes.ts
@@ -1,19 +1,22 @@
 import { Router } from "express";
+import multer from "multer";
 import { TobaccoRatingController, CoalRatingController } from "../controllers";
 import { checkAuth } from "../utils";
 
 const router = Router();
 
+const upload: multer.Multer = multer();
+
 router
   .route("/api/rating/tobacco")
-  .post(checkAuth, TobaccoRatingController.add)
-  .put(checkAuth, TobaccoRatingController.update)
+  .post(checkAuth, upload.none(), TobaccoRatingController.add)
+  .put(checkAuth, upload.none(), TobaccoRatingController.update)
   .delete(checkAuth, TobaccoRatingController.remove);
 
 router
   .route("/api/rating/coal")
-  .post(checkAuth, CoalRatingController.add)
-  .put(checkAuth, CoalRatingController.update)
+  .post(checkAuth, upload.none(), CoalRatingController.add)
+  .put(checkAuth, upload.none(), CoalRatingController.update)
   .delete(checkAuth, CoalRatingController.remove);
 
 export { router };
