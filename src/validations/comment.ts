ba513157ed48558d2ecdf52e81936bc3a4c115e0
Iamaletdinov Artur <wbf.robit@gmail.com>
2023-08-22 09:25:51 +0500

    для валидатора сохраннения комментов длинна uuid вынесена в отдельную переменную

--- src/validations/comment.ts
+++ src/validations/comment.ts
@@ -1,6 +1,9 @@
 import { body } from "express-validator";
 import { handleValidationErrors } from "../utils";
 
+// длинна строки uuid
+const uuidLengthOpt = { min: 36, max: 36 };
+
 const text = body(
   "text",
   "Текст комментария должен сожержать от 10 до 256 символов"
@@ -8,20 +11,14 @@ const text = body(
 
 export const saveCoalComment = [
   text,
-  body("coalId", "Отсутствует id комментируемого угля").isLength({
-    // длинна строки uuid
-    min: 36,
-    max: 36,
-  }),
+  body("coalId", "Отсутствует id комментируемого угля").isLength(uuidLengthOpt),
   handleValidationErrors,
 ];
 
 export const saveTobaccoComment = [
   text,
-  body("tobaccoId", "Отсутствует id комментируемого табака").isLength({
-    // длинна строки uuid
-    min: 36,
-    max: 36,
-  }),
+  body("tobaccoId", "Отсутствует id комментируемого табака").isLength(
+    uuidLengthOpt
+  ),
   handleValidationErrors,
 ];
