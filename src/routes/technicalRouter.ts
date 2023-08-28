966d65b15a2ea211d00685290f16507c509a3628
Iamaletdinov Artur <wbf.robit@gmail.com>
2023-08-28 18:06:20 +0500

    Добавлениы пути для статики админпанели

--- src/routes/technicalRouter.ts
+++ src/routes/technicalRouter.ts
@@ -8,8 +8,17 @@ router.get("/static/:dirName/:fileName", (req: Request, res: Response) => {
   res.sendFile(`client/static/${dirName}/${fileName}`, rootDirNameObj);
 });
 
+router.get("/assets/:dirName/:fileName", (req: Request, res: Response) => {
+  const { dirName, fileName } = req.params;
+  res.sendFile(`adminpanel/assets/${dirName}/${fileName}`, rootDirNameObj);
+});
+
 router.get("/favicon.ico", (_, res: Response) =>
   res.sendFile(`client/favicon.ico`, rootDirNameObj)
+);
+
+router.get("/logo192.png", (_, res: Response) =>
+  res.sendFile(`client/logo192.png`, rootDirNameObj)
 );
 
 router.get("/manifest.json", (_, res: Response) =>
