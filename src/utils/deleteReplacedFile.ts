2b49c44e80d64dbbd775e5ce4c6f9bc995e1fc59
Iamaletdinov Artur <wbf.robit@gmail.com>
2023-10-22 20:50:27 +0500

    удален deleteReplacedFile т.к. дубликат существующего  toDeleteFile

--- src/utils/deleteReplacedFile.ts
+++
@@ -1,8 +0,0 @@
-import fs from "fs";
-import logger from "../logger/logger.service";
-
-export const deleteReplacedFile = (path: string): void => {
-  fs.unlink(path, (err) => {
-    if (err) logger.error(err.message);
-  });
-};
